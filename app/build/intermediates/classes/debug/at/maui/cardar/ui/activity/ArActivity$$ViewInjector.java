23528fe6efc1613660b5b1dd29f9e87be7148902
bernlim93 <me@bernlim.com>
2016-02-20 09:35:55 -0600

    First Commit

---
+++ app/build/intermediates/classes/debug/at/maui/cardar/ui/activity/ArActivity$$ViewInjector.java
@@ -0,0 +1,20 @@
+// Generated code from Butter Knife. Do not modify!
+package at.maui.cardar.ui.activity;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+
+public class ArActivity$$ViewInjector {
+  public static void inject(Finder finder, final at.maui.cardar.ui.activity.ArActivity target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131296257, "field 'overlayView'");
+    target.overlayView = (at.maui.cardar.ui.widget.CardboardOverlayView) view;
+    view = finder.findRequiredView(source, 2131296256, "field 'cardboardView'");
+    target.cardboardView = (com.google.vrtoolkit.cardboard.CardboardView) view;
+  }
+
+  public static void reset(at.maui.cardar.ui.activity.ArActivity target) {
+    target.overlayView = null;
+    target.cardboardView = null;
+  }
+}
